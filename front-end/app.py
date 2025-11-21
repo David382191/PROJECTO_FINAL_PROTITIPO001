d4535927661f9ce5078e014a5369cd038da6cf09
David382191 <espinozalfonsoch@gmail.com>
2025-11-20 21:41:33 -0500

    Sprint3
    Hola, este es el nuevo Sprint del proyecto final.

--- front-end/app.py
+++ front-end/app.py
@@ -1,26 +1,17 @@
 ## Alfonso Espinoza.
-
+###########################################################################################
 ##Aquí primero vamos a traer las librerías que usaremos aquí.
 from flask import Flask, render_template, request, redirect, session
-import mysql.connector
+import mysql.connector ##esto de acá esuna libreria...negro
 
-
+from database.db import get_connection   # <<--- IMPORTAMOS LA BD
+###########################################################################################
 # CREAR APP DE FLASK
 app = Flask(__name__)
 #Esta es una contraseña que usaremos más tarde. 
 app.secret_key = "clave_super_secreta_123"
-
-
-# Conexión a la BD
-conexion = mysql.connector.connect(
-    host="localhost",
-    port=3307,
-    user="root",
-    password="12345",
-    database="base_ejemplo_prueba"
-)
-
-
+###########################################################################################
+###########################################################################################
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -30,39 +21,51 @@ def login():
         #Aquí vamos a necesitar usar consultas SQL, por lo que
         # necesitamremos abrir un lugar donde ejecutarlas,
         # eso lo que hace esto de aquí abajo. 
+        conexion = get_connection()
         cursor = conexion.cursor(dictionary=True)
 
         #de esta forma podemos hacer consultas a la base de datos SQL como esta.
         #Los %s evitan SQL Injection. Mientras que se leeran los valores que enviamos.
-        cursor.execute("SELECT * FROM users a WHERE username=%s AND password=%s",
+        cursor.execute("SELECT * FROM administrador_secretaria a WHERE usuario=%s AND contrasena=%s",
                        (usuario, password))
         
         admin = cursor.fetchone()
+        cursor.close()
+        conexion.close()
 
         ##Aquí es donde funciona el redirecionamiento.
         if admin:
-            session["admin_id"] = admin["id"]
+            session["admin_id"] = admin["idAdmin"]
             ##Si todo está bien, nos enviará a este lugar. Aquí es donde podemos cambiarlo.
-            return redirect("/templates/home")
+            return redirect("/home")
         else:
             return render_template("login.html", error="Usuario o contraseña incorrectos")
 
     return render_template("login.html")
-
-
+###########################################################################################
+###########################################################################################
+@app.route("/home")
+def home():
+    if "admin_id" not in session:
+        return redirect("/")
+    return render_template("home.html")
+###########################################################################################
+###########################################################################################
 @app.route("/panel")
 def panel():
     if "admin_id" not in session:
         return redirect("/")
-
     return render_template("panel.html")
-
-
+###########################################################################################
+###########################################################################################
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
-
-
+###########################################################################################
+###########################################################################################
 if __name__ == "__main__":
     app.run(debug=True)
+###########################################################################################
+###########################################################################################
+##Listo, Roberto, terminamos aquí.
